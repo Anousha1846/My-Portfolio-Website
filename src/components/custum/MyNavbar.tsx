bf70c98a8296f9a2f22d3986749e50ba1cde02bf
Anousha1846 <125111686+Anousha1846@users.noreply.github.com>
2024-11-17 10:40:18 -0500

    Update MyNavbar.tsx

--- src/components/custum/MyNavbar.tsx
+++ src/components/custum/MyNavbar.tsx
@@ -20,7 +20,7 @@ const MyNav = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6  lg:px-8 ">
         <div className="flex items-center justify-between h-16  ">
         <div className="flex items-center">
-              <div className="flex-shrink-0 lg:text-5xl text-4xl  ">
+              <div className="flex-shrink-0 lg:text-4xl text-3xl  ">
                 <Link href="/" className="text-sky-700 italic font-mono font-extrabold">
                   <Popover>
                     <PopoverTrigger>  Anousha</PopoverTrigger>
