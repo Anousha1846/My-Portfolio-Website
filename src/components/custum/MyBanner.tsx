b6dcdedc2a0e1bc1e2e17b80bf5e212bc4c3ea9b
Anousha1846 <125111686+Anousha1846@users.noreply.github.com>
2024-11-17 11:09:55 -0500

    Update MyBanner.tsx

--- src/components/custum/MyBanner.tsx
+++ src/components/custum/MyBanner.tsx
@@ -8,7 +8,7 @@ import { TextGenerateEffect } from "../ui/TextGenerateEffectUI"
 const Banner = () => {
   return (
     <div>
-<div className="relative w-full md:h-[585px]  h-[680px]  ">
+<div className="relative w-full md:h-[585px]  h-[672px]  ">
       {/* Background Image */}
       <Image
         src={BannerImg}  
