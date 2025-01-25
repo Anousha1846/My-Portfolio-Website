d7ba289fd3daebdf4d3e484afe8f63e83aa19f37
Anousha1846 <125111686+Anousha1846@users.noreply.github.com>
2025-01-25 13:00:21 -0500

    Update MyProjects.tsx

--- src/components/custum/MyProjects.tsx
+++ src/components/custum/MyProjects.tsx
@@ -3,6 +3,31 @@ import { FocusCards } from "../ui/FocusCards";
 
 export default function ProjectsPage() {
   const cards = [
+       {
+        title: "PrimePick",
+        description: "PrimePick is a dynamic platform featuring seamless API data handling and secure user authentication via Clerk. It combines responsive design with effortless navigation for an optimized user experience.",
+        githubLink: "https://github.com/Anousha1846/PrimePick",
+        liveLink: "https://prime-pick-11wo-22hlgwzgn-anousha1846s-projects.vercel.app/",
+      },
+        {
+      title: "MORENT-Rent A Car",
+      description: "MORENT is a cutting-edge car rental platform, designed with a user-friendly interface and smooth functionality, ensuring a hassle-free and efficient car booking experience for users.",
+      githubLink: "https://github.com/Anousha1846/Hackathon3-GIAIC_Morent",
+      liveLink: "https://hackathon3-giaic-morent.vercel.app/" ,
+    },
+       {
+        title: "Food Blog Website",
+        description: "My Food Blog website offers dynamic routing for intuitive exploration and an engaging comment section, fostering vibrant interaction and effortless discovery of culinary delights.",
+        githubLink: "https://github.com/Anousha1846/Anousha-s-Food-Blog",
+        liveLink: "https://anousha-s-food-blog-7cle-l8vwzw069-anousha1846s-projects.vercel.app/",
+      },
+ {
+      title: "Portfolio",
+      description: "My portfolio showcases innovative and professional projects, highlighting creative designs, responsive layouts, and seamless functionality for an exceptional user experience.",
+      githubLink: "https://github.com/Anousha1846/Portfolio-2",
+      liveLink: "https://project-1-p4x1nhwyy-anousha1846s-projects.vercel.app/",
+    },
+    
     {
       title: "DWELL-House Website",
       description: "DWELL-house is a sleek, responsive platform showcasing residential architecture projects. Built with Next.js and Tailwind CSS, it offers an intuitive layout, highlighting project details and providing a seamless, visually engaging browsing experience.",
@@ -15,36 +40,30 @@ export default function ProjectsPage() {
       githubLink: "https://github.com/Anousha1846/ElectroMart",
       liveLink: "https://electro-mart-topaz.vercel.app/",
     },
-    
-    {
-      title: "Simple Static Resume Builder",
-      description: "Simple Static Resume Builder is a user-friendly tool that enables users to create elegant resumes effortlessly. It features customizable templates, easy input forms, and a clean layout for professional presentation.",
-      githubLink: "https://github.com/Anousha1846/Simple-resume-builder-1",
-      liveLink: "https://anousha1846.github.io/Simple-resume-builder-1/",
-    },
+     {
+        title: "NEXCENT-Figma Template",
+        description: "NEXCENT is a polished Figma template showcasing a modern and visually appealing UI design, perfect for inspiring seamless user experiences and creative project layouts."
+        githubLink: "https://github.com/Anousha1846/Figma-Template",
+        liveLink: "https://figma-template-cdt5y7n80-anousha1846s-projects.vercel.app/",
+      },
     {
         title: "Shopping 2.0",
         description: "This eCommerce website offers a seamless shopping experience with an intuitive add-to-cart functionality. Users can easily browse products, manage their cart, and enjoy secure checkout for a hassle-free purchase process.",
         githubLink: "https://github.com/Anousha1846/Hackathon-1.2",
         liveLink: "https://anousha1846.github.io/Hackathon-1.2/",
       },
-    {
-      title: "Pokemon Clone",
-      description: "This Pokémon clone website, developed during my CSS learning, showcases basic styling and JavaScript functionality. While unresponsive, it highlights my foundational skills in web design and interactive elements.",
-      githubLink: "https://github.com/Anousha1846/Pokemon-Clone",
-      liveLink: "https://anousha1846.github.io/Pokemon-Clone/",
-    },
+
     {
         title: "Royal Rides",
         description: "Royal Rides is a simple, elegant website built using only HTML and CSS. It showcases sleek design, emphasizing user-friendly navigation, presenting services, and enhancing the experience of exploring luxurious ride options.",
         githubLink: "https://github.com/Anousha1846/RoyalRides",
         liveLink: "https://anousha1846.github.io/RoyalRides/",
       },
-    {
-        title: "Static Marksheet",
-        description: "This static marksheet project, created with JavaScript, provides a simple interface for displaying student grades. Users can view scores clearly without dynamic input, ensuring an easy and straightforward experience.",
-        githubLink: "https://github.com/Anousha1846/marksheet-js",
-        liveLink: "https://anousha1846.github.io/marksheet-js/",
+         {
+        title: "Dynamic Editable Resume Builder",
+        description: "A user-friendly resume builder offering dynamic editing and instant downloading. Easily customize fields like contact info, education, and experience to create a polished, professional resume tailored to your needs.",
+        githubLink: "https://github.com/Anousha1846/Updated-Resume-Builder-5",
+        liveLink: "https://updated-resume-builder-5.vercel.app/",
       },
       {
         title: "TO-DO App",
@@ -52,19 +71,24 @@ export default function ProjectsPage() {
         githubLink: "https://github.com/Anousha1846/ToDo-list",
         liveLink: "https://anousha1846.github.io/ToDo-list/",
       },
-      {
-        title: "Dynamic Editable Resume Builder",
-        description: "A user-friendly resume builder offering dynamic editing and instant downloading. Easily customize fields like contact info, education, and experience to create a polished, professional resume tailored to your needs.",
-        githubLink: "https://github.com/Anousha1846/Updated-Resume-Builder-5",
-        liveLink: "https://updated-resume-builder-5.vercel.app/",
-      },
+       {
+      title: "Simple Static Resume Builder",
+      description: "Simple Static Resume Builder is a user-friendly tool that enables users to create elegant resumes effortlessly. It features customizable templates, easy input forms, and a clean layout for professional presentation.",
+      githubLink: "https://github.com/Anousha1846/Simple-resume-builder-1",
+      liveLink: "https://anousha1846.github.io/Simple-resume-builder-1/",
+    },
       {
         title: "Netflix Home Page Clone",
         description: "Netflix Homepage Clone is a responsive website built using HTML and Tailwind CSS. It replicates the Netflix interface, featuring a sleek design, dynamic content display, and smooth navigation for an immersive experience.",
         githubLink: "https://github.com/Anousha1846/Netflix-Homepage-Clone",
         liveLink: "https://anousha1846.github.io/Netflix-Homepage-Clone/",
       },
-      
+        {
+        title: "Static Marksheet",
+        description: "This static marksheet project, created with JavaScript, provides a simple interface for displaying student grades. Users can view scores clearly without dynamic input, ensuring an easy and straightforward experience.",
+        githubLink: "https://github.com/Anousha1846/marksheet-js",
+        liveLink: "https://anousha1846.github.io/marksheet-js/",
+      },
       
   ];
 
